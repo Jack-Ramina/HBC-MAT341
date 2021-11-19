7b10d2788f0801b88bf7b27934bf9a083bb12e80
Jack Ramina <jackramina@gmail.com>
2021-11-19 11:56:22 -0500

    Updated the algorithm. Improved speed by removing the need for cost_Func. Added a plot. Added an rng seed so partner and professor have same random values

--- Simulated_Annealing/simulated_Annealing.m
+++ Simulated_Annealing/simulated_Annealing.m
@@ -1,17 +1,24 @@
-function simulated_Annealing()
+function simulated_Annealing2()
+load('Cost_Matrix.mat')
+rng(30)
 
-guess = 1:1:1000;
-frac = 7/8;
-tol = 10;
-temp = 1000;
-k = 0.0001;
-max_iter = 100;
+guess = randperm(1000);
+frac = 0.975;
+tol = 1e-3;
+temp = 1e3;
+k = 0.01;
+max_iter = 10000;
 count = 0;
+
+cost = 0;
+for j = 1:length(guess)-1
+    cost = cost + A(guess(j), guess(j+1));
+end
+        
 
 while temp > tol
     for i = 1:max_iter
         count = count + 1;
-        cost = cost_Func(guess);
         
         guessTemp = guess;
         q = randi(999);
@@ -19,23 +26,31 @@ while temp > tol
         guessTemp(q) = guessTemp(q+1);
         guessTemp(q+1) = swap;
         
-        newCost = cost_Func(guessTemp);
+        newCost = 0;
+        for m = 1:length(guessTemp)-1
+            newCost = newCost + A(guessTemp(m),guessTemp(m+1));
+        end
         deltaC = newCost - cost;
         
         if deltaC < 0
             guess = guessTemp;
+            cost = newCost;
         else
             ex = exp((-deltaC)/(k*temp));
             r = rand();
             if r < ex
                 guess = guessTemp;
+                cost = newCost;
             end
         end
         
-        costs(count) = cost_Func(guess);
+        costs(count) = cost;
+        counts(count) = count;
     end
     temp = frac * temp;
 end
-costs
 guess'
 cost_Func(guess)
+
+figure(1)
+plot(counts,costs)
